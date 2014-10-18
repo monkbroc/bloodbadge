7845bafdecf6d5fcdf1b1c19eb01e5a51c3ed333
Julien Vanier <jvanier@gmail.com>
2014-10-18 17:34:09 -0400

    Fixed flash and devise messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def flash_class(level)
+    "flash alert alert-dismissable " +
+    case level
+      when :notice then "alert-info"
+      when :success then "alert-success"
+      else "alert-danger"
+    end
+  end
+
 end
